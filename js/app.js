4962c225f43443cb3094fd1dc63452557f22f0ff
Alpi Rimppi <alpi@alpiini.com>
2018-02-07 23:41:45 +0200

    Semi-working stuff

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation();
+
+
+$(window).bind('scroll', function () {
+    h = ($( "#topslide" ).height())
+    if ($(window).scrollTop() > h) {
+        $('#menucontainer').addClass('fixed');
+        $('#content').css('margin-top', '50px');
+    } else {
+        $('#menucontainer').removeClass('fixed');
+        $('#content').css('margin-top', '0')
+    }
+});

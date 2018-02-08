20fb0a66a79b43cd83c06d2abdaa7311d3942cd9
Alpi Rimppi <alpi@alpiini.com>
2018-02-08 12:55:41 +0200

    Smoother stickymenu

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@ $(document).foundation();
 
 $(window).bind('scroll', function () {
     h = ($( "#topslide" ).height())
+    menuh = ($( "#menucontainer" ).height()) + 15
     if ($(window).scrollTop() > h) {
         $('#menucontainer').addClass('fixed');
-        $('#content').css('margin-top', '50px');
+        $('#content').css('margin-top', menuh);
     } else {
         $('#menucontainer').removeClass('fixed');
         $('#content').css('margin-top', '0')
